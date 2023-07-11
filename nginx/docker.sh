ff855a3697bb927edd0b440f26c969bb5ac28505
John J <john@xoren.io>
2023-07-11 04:16:51 +0100

    nginx docker.sh update

--- nginx/docker.sh
+++ nginx/docker.sh
@@ -2,6 +2,7 @@
 
 block_bots=${NGINX_BLOCK_BOTS:-"0"}
 block_scanners=${NGINX_BLOCK_SCANNERS:-"0"}
+swoole_port=${SWOOLE_PORT:-8000}
 
 # Function to check if the openssl commands have finished
 check_openssl_commands() {
@@ -71,18 +72,18 @@ config_file="/etc/nginx/conf.d/api.conf"
 
 current_port=$(grep -Po "server laravel:\K\d+" "$config_file")
 
-if [[ "$current_port" != "${SWOOLE_PORT:-8000}" ]]; then
-    sed -i "s/server laravel:$current_port/server laravel:${SWOOLE_PORT:-8000}/g" "$config_file"
-    sed -i "s/server localhost:$current_port backup/server localhost:${SWOOLE_PORT:-8000} backup/g" "$config_file"
+if [[ "$current_port" != "$swoole_port" ]]; then
+    sed -i "s/server laravel:$current_port/server laravel:$swoole_port/g" "$config_file"
+    sed -i "s/server localhost:$current_port backup/server localhost:$swoole_port backup/g" "$config_file"
 fi
 
 # Check if port ${SWOOLE_PORT:-8000} is open on laravel DNS using netcat, Kubernetes friendly
-while ! nc -z -w1 laravel ${SWOOLE_PORT:-8000} && ! nc -z -w1 localhost ${SWOOLE_PORT:-8000}; do
-    echo "[ERROR] http://laravel:${SWOOLE_PORT:-8000} is CLOSED."
+while ! nc -z -w1 laravel "$swoole_port" && ! nc -z -w1 localhost "$swoole_port"; do
+    echo "[ERROR] http://laravel:$swoole_port is CLOSED."
     sleep 3s
 done
 
-echo "[SUCCESS] http://laravel:${SWOOLE_PORT:-8000} is OPEN."
+echo "[SUCCESS] http://laravel:$swoole_port is OPEN."
 
 # Start crond in the background
 crond -l 2 -b
